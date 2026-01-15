faeaf4cb2bde2ecd8952c5ca7561711043577aa5
weew12 <weew12@2528566339@qq.com>
2026-01-15 08:52:31 +0000

    2 修改缩容到0相关逻辑

--- pkg/handlers/informers.go
+++ pkg/handlers/informers.go
@@ -64,7 +64,8 @@ func applyValidation(deployment *appsv1.Deployment, kubeClient *kubernetes.Clien
 	current := *deployment.Spec.Replicas
 	var target int
 	if current == 0 {
-		target = 1
+		// target = 1
+		target = 0 // 原本的逻辑是当实例数为 0 的时候 自动改为1 必须改为 0 否则无法实现函数实例为 0
 	} else if current > MaxReplicas {
 		target = MaxReplicas
 	} else {
